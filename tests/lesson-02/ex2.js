4974cf667a6520797a22b961d102f40aa6e6f313
nhiha98-lab <nhiha.98@gmail.com>
2026-09-15 22:11:55 +0700

    bai tap ve nha

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,7 @@
+// const myName = "Alex";
+// myName = "Nagi";
+// console.log(myName);
+
+let myName = "Alex";
+myName = "Nagi";
+console.log(myName);
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,6 @@
+const height = 167;
+const extHeight = height % 100;
+const idealWeight = extHeight * 9 / 10;
+const maxWeight = extHeight;
+const minWeight = extHeight * 8 / 10;
+console.log(idealWeight, maxWeight, minWeight);
